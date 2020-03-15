613ae157bf9527555f0188ea7dd19ad4bad26b8d
Alexlogvin2019 <alexlogvin@yahoo.com>
2020-03-15 14:37:42 +0200

    Updates

---
+++ precache-manifest.9b79f406e409be9adeaee735bf2fd675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca93c42ea5bcf3e832158fc0ae7e325",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "3b6d5c32261015c12b29",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0a185486397da1b44022",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.1f934de3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.1f934de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b6d5c32261015c12b29",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.49917f55.chunk.js"
+  },
+  {
+    "revision": "873f82bc3a1b5f048e84",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.b2026c57.js"
+  }
+]);
